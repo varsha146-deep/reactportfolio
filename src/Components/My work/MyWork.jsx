f0f987915efea584ec18dc41f76b78f27642fc06
varsha146-deep <varshavinayan016@gmail.com>
2025-11-26 21:07:41 +0530

    portfolio

--- src/Components/My work/MyWork.jsx
+++ src/Components/My work/MyWork.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import './MyWork.css'
 import theme_pattern1 from '../../assets/theme_pattern1.jpg'
-import mywork_data from '../../assets/MyWork_data.js'
+import mywork_data from '../../assets/mywork_data.js'
 import arrow_icon from '../../assets/arrow_icon.jpg'
 
 function MyWork() {
   return (
-    <div id='work' className='mywork'>
+    <div  className='mywork'>
       <div className="mywork-title">
         <h1>My Latest Work</h1>
         <img src={theme_pattern1} className='theme' alt="" />
